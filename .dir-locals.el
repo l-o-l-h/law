8591dcdb40de08c2cba7e46bbe6f7502817222e1
Wesley Harvey <wlharvey4@mac.com>
2024-10-28 09:09:22 -0700

    New and updated law notes 2024-10-28T0900

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;;
 ;;;     (info "(emacs) Directory Variables")
 
-((nil . ((denote-directory . "~/.local/share/notes/law"))))
+((nil . ((denote-directory . "~/.local/share/notes/law")))
+ (org . ((org-publish-project-alist
+          (("law"
+             :base-directory "~/.local/share/notes/law/source"
+             :publishing-directory "~/.local/share/notes/law/publish"))))))
